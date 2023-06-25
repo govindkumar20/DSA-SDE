47325600f1cd38095b48a92a012121d156f22e53
Govind Kumar <120386156+govindkumar20@users.noreply.github.com>
2023-06-25 21:16:12 +0530

    Time: 434 ms (17.83%), Space: 49.5 MB (13.98%) - LeetHub

---
+++ 0018-4sum/0018-4sum.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    vector<vector<int>> fourSum(vector<int>& nums, int target) {
+        int n=nums.size();
+        set<vector<int>> s;
+        vector<vector<int>> ans;
+        sort(nums.begin(),nums.end());
+        for(int i=0;i<n;i++)
+        {
+            for(int j=i+1;j<n;j++)
+            {
+                int k=j+1;
+                int e=n-1;
+                while(k<e)
+                {
+                    long long sum=(long long)nums[i]+(long long)nums[j]+(long long)nums[k]+(long long)nums[e];
+                    if(sum==target)
+                    {
+                        s.insert({nums[i],nums[j],nums[k],nums[e]});
+                        k++;
+                        e--;
+                    }
+                    else if(sum<target)
+                    {
+                        k++;
+                    } else{
+                        e--;
+                    }
+                }
+            }
+        }
+        for(auto i: s)
+        {
+        ans.push_back(i);
+        }
+        return ans;
+    }
+};
