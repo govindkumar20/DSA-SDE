110d3dc5154bfe5faaeb1eaad1c5d4c90ae4b6ec
Govind Kumar <120386156+govindkumar20@users.noreply.github.com>
2023-06-26 14:36:48 +0530

    Time: 149 ms (52.53%), Space: 93.4 MB (30.27%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -5,7 +5,7 @@ public:
         int maxprofit=0;
 
         // store min stock value and  sell stock everyday to find max profit
-        for(int i=0;i<prices.size();i++)
+        for(int i=1;i<prices.size();i++)
         {
             minsofar=min(minsofar,prices[i]);       
             maxprofit=max(maxprofit,prices[i]-minsofar);
