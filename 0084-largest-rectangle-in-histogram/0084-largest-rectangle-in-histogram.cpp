e45d2be83863c1019c85bdb20e4669d664c4714b
Govind Kumar <120386156+govindkumar20@users.noreply.github.com>
2023-07-01 00:09:25 +0530

    Time: 229 ms (44.31%), Space: 81.3 MB (61.43%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
@@ -1,50 +1,36 @@
 class Solution {
 public:
     int largestRectangleArea(vector<int>& heights) {
-        int n=heights.size();
-        int leftmin[n];
-        int rightmin[n];
-            int maxans=0;
-            stack<int> st;
-            for(int i=0;i<n;i++)
-            {
-                while(!st.empty() && heights[st.top()]>=heights[i])
-                {
-                    st.pop();
-                }
-                if(st.empty())
-                {
-                    leftmin[i]=0;
-                } else
-                {
-                      leftmin[i]=st.top()+1;
-                }
-                st.push(i);
-                 }
-                 while(!st.empty())
-                {
-                    st.pop();
-                }
-                for(int i=n-1;i>=0;i--)
-            {
-                while(!st.empty() && heights[st.top()]>=heights[i])
-                {
-                    st.pop();
-                }
-                if(st.empty())
-                {
-                    rightmin[i]=n-1;
-                } else
-                {
-                      rightmin[i]=st.top()-1;
-                }
-                st.push(i);
-                 }
-                 for(int i=0;i<n;i++)
-                 {
-                     maxans=max(maxans,(rightmin[i]-leftmin[i]+1)*heights[i]);
-                     
-                 }
-                 return maxans;
+        int n = heights.size();
+        vector<int> ps(n);  
+        vector<int> ns(n);  
+        stack<int> st;
+        int ans = 0;
+
+        for (int i = 0; i < n; i++) {
+            while (!st.empty() && heights[st.top()] >= heights[i]) {
+                st.pop();
+            }
+            ps[i] = (st.empty()) ? -1 : st.top();
+            st.push(i);
+        }
+
+        while (!st.empty()) {
+            st.pop();
+        }
+
+        for (int i = n - 1; i >= 0; i--) {
+            while (!st.empty() && heights[st.top()] >= heights[i]) {
+                st.pop();
+            }
+            ns[i] = (st.empty()) ? n : st.top();
+            st.push(i);
+        }
+
+        for (int i = 0; i < n; i++) {
+            ans = max(ans, (ns[i] - ps[i] - 1) * heights[i]);
+        }
+
+        return ans;
     }
 };
