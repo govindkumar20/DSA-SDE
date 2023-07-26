cd840e839b1cb4e113fc5a21c622843d8188e605
Govind Kumar <120386156+govindkumar20@users.noreply.github.com>
2023-07-27 00:13:14 +0530

    Time: 649 ms (27.93%), Space: 60.8 MB (71.89%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
@@ -4,19 +4,19 @@ public:
         int n=nums.size();
         int maxlen=0;
         unordered_set<int> s(nums.begin(),nums.end());
-        for( auto i: nums)
+        for(auto i:nums)
         {
             int curr=i;
             int prev=i-1;
             if(s.find(prev)==s.end())
             {
-               int currlen=1;
-               while(s.find(curr+1)!=s.end())
-               {
-                   currlen++;
-                   curr++;
-               }
-               maxlen=max(maxlen,currlen);
+                int currlen=1;
+                while(s.find(curr+1)!=s.end())
+                {
+                    currlen++;
+                    curr++;
+                }
+                maxlen=max(maxlen,currlen);
             }
         }
         return maxlen;
