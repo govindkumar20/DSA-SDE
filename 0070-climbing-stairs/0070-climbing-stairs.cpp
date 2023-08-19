669a903a84522ea80c4b32215f14701d2fcf7e2c
Govind Kumar <120386156+govindkumar20@users.noreply.github.com>
2023-08-19 14:09:36 +0530

    Time: 2 ms (40.59%), Space: 6.1 MB (38.74%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+        int prev=1;
+        int prev2=1;
+        for(int i=2;i<=n;i++)
+        {
+           int curri=prev+prev2;
+            prev2=prev;
+        prev=curri;
+
+        }
+        return prev;
+    }
+};
