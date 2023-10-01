71268fde0879b6f321e5ed864f402e73e7f042c2
Govind Kumar <120386156+govindkumar20@users.noreply.github.com>
2023-10-01 21:31:36 +0530

    Time: 3 ms (90.69%), Space: 13.3 MB (40.19%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -4,7 +4,7 @@ public:
         long maxprofit=0;
         for(int i=1;i<prices.size();i++)
         {
-            if(prices[i]>prices[i-1])
+            if(prices[i]>=prices[i-1])
             {
                 maxprofit+=prices[i]-prices[i-1];
             }
