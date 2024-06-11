e5296dcade5301cf3883acc5ba87b450f2ac66f9
Govind Kumar <120386156+govindkumar20@users.noreply.github.com>
2024-06-11 14:53:45 +0530

    Time: 38 ms (64.39%), Space: 39.8 MB (77.05%) - LeetHub

---
+++ 0852-friends-of-appropriate-ages/0852-friends-of-appropriate-ages.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int numFriendRequests(vector<int>& ages) {
+    
+        int a[121] = {}, res = 0;
+  for (auto age : ages) ++a[age];
+  for (auto i = 15; i <= 120; ++i)
+    for (int j = 0.5 * i + 8; j <= i; ++j) res += a[j] * (a[i] - (i == j));
+  return res;
+    }
+};
