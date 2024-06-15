87bc31e277128baaecce7b5250bf660d5d389138
Govind Kumar <120386156+govindkumar20@users.noreply.github.com>
2024-06-15 21:36:58 +0530

    Time: 290 ms (24.79%), Space: 16.1 MB (67.67%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -1,28 +1,26 @@
 class Solution {
 public:
     bool canPartition(vector<int>& nums) {
-        int sum=0;
+        //s+s=2s
+        // so find whether there exist a subset with sum=sum/2
         int n=nums.size();
-        for(auto i:nums)
-        {
-            sum+=i;
-        }
+        int sum=0;
+        for(auto i:nums) sum+=i;
         if(sum%2!=0) return false;
-        sum/=2;
-       
-       vector<vector<bool>> dp(n,vector<bool>(sum+1,0));
-    for(int i=0;i<n;i++) dp[i][0]=true;
-    if(nums[0]<=sum) dp[0][nums[0]]=true;
-    for(int i=1;i<n;i++)
-    {
-        for(int j=1;j<=sum;j++)
-        {
-            bool notTake=dp[i-1][j];
-            bool take=false;
-            if(nums[i]<=j) take=dp[i-1][j-nums[i]];
-            dp[i][j]=take || notTake;
+        int m=sum/2;
+        vector<vector<bool>> dp(n+1,vector<bool>(m+1));
+        for(int i=0;i<=n;i++) dp[i][0]=true;
+        for(int j=1;j<=m;j++) dp[0][j]=false;
+        // filling up of dp table
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                if(nums[i-1]<=j){
+                    dp[i][j]=dp[i-1][j-nums[i-1]] || dp[i-1][j];
+                } else{
+                    dp[i][j]=dp[i-1][j];
+                }
+            }
         }
-    }
-    return dp[n-1][sum];
+       return dp[n][m];
     }
 };
