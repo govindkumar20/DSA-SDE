701aecf6bdb0dd2ebb4db62ff923b2157f5c6e82
Govind Kumar <120386156+govindkumar20@users.noreply.github.com>
2024-06-18 13:35:37 +0530

    Time: 14 ms (39.59%), Space: 20 MB (31.18%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.cpp
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -1,12 +1,13 @@
 class Solution {
 public:
     int change(int amount, vector<int>& coins) {
+        //unbounded knapsack
         int n=coins.size();
         int m=amount;
         vector<vector<int>> dp(n+1,vector<int>(m+1));
         for(int i=0;i<=n;i++) dp[i][0]=1;
         for(int j=1;j<=m;j++) dp[0][j]=0;
-        for(int i=1;i<=n;i++){
+        for(int i=1;i<=n;i++){ //filling up of dp matrix
             for(int j=1;j<=m;j++){
                 if(coins[i-1]<=j){
                     dp[i][j]=dp[i][j-coins[i-1]]+ dp[i-1][j];
