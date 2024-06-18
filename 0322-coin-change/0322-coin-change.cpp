4b281b682f58e8abd0cb18b0217dfced16130b0a
Govind Kumar <120386156+govindkumar20@users.noreply.github.com>
2024-06-18 18:34:25 +0530

    Time: 33 ms (57.01%), Space: 50.9 MB (15.36%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,15 +1,27 @@
 class Solution {
 public:
     int coinChange(vector<int>& coins, int amount) {
-        vector<int> dp(amount+1,amount+1);
-        dp[0]=0; // 0 amount will require 0 coins
-        for(int i=1;i<=amount;i++)
-        {
-            for(auto c:coins)
-            {
-                if(c<=i) dp[i]=min(dp[i],dp[i-c]+1);
+        int n=coins.size();
+        int m=amount;
+        vector<vector<int>> dp(n+1,vector<int>(m+1));
+        for(int i=0;i<=n;i++) dp[i][0]=0;
+        for(int j=1;j<=m;j++) dp[0][j]=INT_MAX-1; // infinite no of coins are required  eg:-4/3
+        // for(int j=1;j<=m;j++){
+        //     if(j%coins[0]==0) dp[1][j]=j/coins[0];
+        //     else dp[1][j]=INT_MAX-1;
+        // }
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                if(coins[i-1]<=j){
+                    dp[i][j]=min(1+dp[i][j-coins[i-1]],dp[i-1][j]); //+1 bcz if we consider a coint increment coins by one
+                } else{
+                    dp[i][j]=dp[i-1][j];
+                }
+
+
             }
+
         }
-        return  dp[amount]>amount?-1:dp[amount];
+        return dp[n][m]==INT_MAX-1?-1:dp[n][m];
     }
 };
