95313d1ee54df64d6a741d7345991a28f622016f
Govind Kumar <120386156+govindkumar20@users.noreply.github.com>
2024-06-21 01:40:30 +0530

    Time: 209 ms (27.33%), Space: 12.1 MB (25.53%) - LeetHub

--- 0312-burst-balloons/0312-burst-balloons.cpp
+++ 0312-burst-balloons/0312-burst-balloons.cpp
@@ -1,11 +1,11 @@
 class Solution {
 public:
       int solve(vector<int> & a,vector<vector<int>> & dp, int i, int j){
-        if(i>=j) return 0;
+        if(i>j) return 0;
         int ans=INT_MIN;
         if(dp[i][j]!=-1) return dp[i][j];
-        for(int k=i;k<j;k++){
-        int  temp_ans=(a[i-1]*a[k]*a[j])+solve(a,dp,i,k)+solve(a,dp,k+1,j);
+        for(int k=i;k<=j;k++){
+        int  temp_ans=(a[i-1]*a[k]*a[j+1])+solve(a,dp,i,k-1)+solve(a,dp,k+1,j);
             ans=max(ans,temp_ans);
         }
         return dp[i][j]=ans;
@@ -18,6 +18,6 @@ public:
         v.push_back(1);
         int n=v.size();
         vector<vector<int>> dp(n+1,vector<int>(n+1,-1));
-        return solve(v,dp,1,n-1);
+        return solve(v,dp,1,n-2);
     }
 };
