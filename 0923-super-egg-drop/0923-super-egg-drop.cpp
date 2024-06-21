e3eb2313f84fa7f1eb35375a1696686f575cf20a
Govind Kumar <120386156+govindkumar20@users.noreply.github.com>
2024-06-21 15:25:26 +0530

    Time: 51 ms (46.8%), Space: 30.7 MB (62.49%) - LeetHub

--- 0923-super-egg-drop/0923-super-egg-drop.cpp
+++ 0923-super-egg-drop/0923-super-egg-drop.cpp
@@ -1,11 +1,12 @@
 class Solution {
 public:
     int solve(int e,int f,vector<vector<int>>& dp){
+        // brain damaging problem
         if(f==0 || f==1) return f;
         if(e==1) return f;
         int ans=INT_MAX;
         if(dp[e][f]!=-1) return dp[e][f];
-        int s=1;int end=f;
+        int s=1;int end=f;  // use BS to avoid TLE
         while(s<=end)
         {
             int k=s+(end-s)/2;
@@ -13,7 +14,7 @@ public:
             int b=solve(e,f-k,dp);
             int temp_ans=1+max(a,b);
             ans=min(ans,temp_ans);
-            if(a>b) end=k-1;
+            if(a>b) end=k-1; // bcz we want the worst case
             else s=k+1;
         }
         return dp[e][f]=ans;
