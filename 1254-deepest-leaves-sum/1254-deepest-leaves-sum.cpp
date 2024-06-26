958f71f3aeebf21fdfad44f53e0442a9a4495bdb
Govind Kumar <120386156+govindkumar20@users.noreply.github.com>
2024-06-26 16:48:08 +0530

    Time: 69 ms (71.5%), Space: 63.8 MB (5.6%) - LeetHub

---
+++ 1254-deepest-leaves-sum/1254-deepest-leaves-sum.cpp
@@ -0,0 +1,27 @@
+
+class Solution {
+public:
+queue<TreeNode*> q;
+vector<vector<int>> v;
+    int deepestLeavesSum(TreeNode* root) {
+        
+        q.push(root);
+        while(!q.empty()){
+            int n=q.size();
+            vector<int> level;
+            for(int i=0;i<n;i++){
+            TreeNode* node=q.front();
+            q.pop();
+            if(node->left) q.push(node->left);
+            if(node->right) q.push(node->right);
+            level.push_back(node->val);
+            }
+            v.push_back(level);
+        }
+        int sum=0;
+        vector<int> temp=v.back();
+        cout<<v.size();
+        for(auto i:temp) sum+=i;
+        return sum;
+    }
+};
