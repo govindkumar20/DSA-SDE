9c65b0fe86f9d274d99ae5124b5d231cd5568e8b
Govind Kumar <120386156+govindkumar20@users.noreply.github.com>
2024-06-27 22:46:17 +0530

    Time: 229 ms (85.85%), Space: 179.1 MB (35.34%) - LeetHub

--- 1568-pseudo-palindromic-paths-in-a-binary-tree/1568-pseudo-palindromic-paths-in-a-binary-tree.cpp
+++ 1568-pseudo-palindromic-paths-in-a-binary-tree/1568-pseudo-palindromic-paths-in-a-binary-tree.cpp
@@ -13,18 +13,18 @@ class Solution {
 public:
     void solve(TreeNode* root,vector<int> & ans,int & count){
         if(root==NULL) return;
-        ans[root->val]++;
+        ans[root->val]++; //inr the corresponding index value
         solve(root->left,ans,count);
         solve(root->right,ans,count);
         
-        if(root->left==NULL && root->right==NULL) {
+        if(root->left==NULL && root->right==NULL) {  // when we reached leaf node ie one path is completed
             int flag=0;
             for(int i=0;i<=9 ;i++){
-                if(ans[i]%2!=0)flag++;
+                if(ans[i]%2!=0)flag++; // if no of nodes with odd occurences greater than 1 then no permutation can form palindrome
             }
                 if(flag==0 || flag==1) count++; 
         }  
-        ans[root->val]--;
+        ans[root->val]--;// as we backtrack dcr the incremented indices values for further cal of more paths
     }
 
 
@@ -32,7 +32,7 @@ public:
 
     int pseudoPalindromicPaths (TreeNode* root) {
         int count=0;
-        vector<int> ans(10,0);
+        vector<int> ans(10,0);  // vector containing indices of values from 1-9 
         solve(root,ans,count);
         return count;
     }
