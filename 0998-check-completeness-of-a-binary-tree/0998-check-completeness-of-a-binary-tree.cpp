6d46d32bc34a59747da1f8ea7f35163fda4ca211
Govind Kumar <120386156+govindkumar20@users.noreply.github.com>
2024-07-03 20:23:55 +0530

    Time: 3 ms (69.77%), Space: 13 MB (27.25%) - LeetHub

--- 0998-check-completeness-of-a-binary-tree/0998-check-completeness-of-a-binary-tree.cpp
+++ 0998-check-completeness-of-a-binary-tree/0998-check-completeness-of-a-binary-tree.cpp
@@ -12,6 +12,8 @@
 class Solution {
 public:
     bool isCompleteTree(TreeNode* root) {
+        // intution: in a complete BT after first null node  there will be no non null node 
+        // the size of queue will be null after traversal
         bool flag=0;
         queue<TreeNode*> q;
         q.push(root);
