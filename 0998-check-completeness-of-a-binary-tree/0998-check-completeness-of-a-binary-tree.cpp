f93594a94462e4bf609c0244f73b72d0f61aaa98
Govind Kumar <120386156+govindkumar20@users.noreply.github.com>
2024-07-03 20:22:33 +0530

    Time: 8 ms (11.55%), Space: 12.7 MB (91.18%) - LeetHub

---
+++ 0998-check-completeness-of-a-binary-tree/0998-check-completeness-of-a-binary-tree.cpp
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool isCompleteTree(TreeNode* root) {
+        bool flag=0;
+        queue<TreeNode*> q;
+        q.push(root);
+        while(!q.empty()){
+            int n=q.size();
+            for(int i=0;i<n;i++){
+                TreeNode* node=q.front();
+                q.pop();
+                if(node==NULL) flag=1;
+                else{
+                    if(flag) return false;
+                     q.push(node->left);
+                q.push(node->right);
+                }
+               
+
+            }
+        }
+        return true;
+    }
+};
