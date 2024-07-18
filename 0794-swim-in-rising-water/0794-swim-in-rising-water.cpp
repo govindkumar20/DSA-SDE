3f4df29e5dedfc93af5c0a24e8b03ffd76c82d29
Govind Kumar <120386156+govindkumar20@users.noreply.github.com>
2024-07-19 00:42:37 +0530

    Time: 7 ms (95.08%), Space: 12.8 MB (53.43%) - LeetHub

---
+++ 0794-swim-in-rising-water/0794-swim-in-rising-water.cpp
@@ -0,0 +1,35 @@
+
+#define pair pair<int, pair<int,int>>
+class Solution {
+public:vector<int> xm={-1,0,1,0}; // 4 drn for travelling
+        vector<int> ym={0,1,0,-1};
+    int swimInWater(vector<vector<int>>& heights) {
+          int n=heights.size();
+        int m=heights[0].size();
+        vector<vector<int>> vis(n,vector<int>(m,1e9));
+        priority_queue<pair,vector<pair>,greater<pair>> q; // min_heap bcz we want min effort to stay at top
+        vis[0][0]=0; // initial constraints
+        q.push({heights[0][0],{0,0}});
+        while(!q.empty()){
+            int r=q.top().second.first;
+            int c=q.top().second.second;
+            int d=q.top().first;
+            q.pop();
+            if(r==n-1 && c==m-1) return d;
+            for(int i=0;i<4;i++){
+                int nr=r+xm[i];
+                int nc=c+ym[i];
+                if(nr>=0 && nr<n && nc>=0 && nc<m ){
+                    int nd=max(d,heights[nr][nc]); // we want max absolute dist between adj cells
+                    if(nd<vis[nr][nc]){  // if that dist is less then present val then present value = that dist
+                        vis[nr][nc]=nd; 
+                        q.push({nd,{nr,nc}}); // push that in q
+                    }
+                }
+            }
+
+
+        }
+        return 0;
+    }
+};
