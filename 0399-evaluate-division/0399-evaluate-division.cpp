d38172998f6e83b551f80371431cf33a21dee3c0
Govind Kumar <120386156+govindkumar20@users.noreply.github.com>
2024-07-20 18:46:40 +0530

    Time: 0 ms (100%), Space: 11.4 MB (33.95%) - LeetHub

---
+++ 0399-evaluate-division/0399-evaluate-division.cpp
@@ -0,0 +1,34 @@
+// initially i cannot fuckin imagine this is a graph problem
+
+
+class Solution {
+public:
+   double dfs(string up, string down, unordered_map<string,unordered_map<string,double>>& m, unordered_set<string>& visited) {
+    if(m[up].find(down)!=m[up].end()) return m[up][down];
+    for(auto i:m[up]){
+        if(visited.find(i.first)==visited.end()){
+            visited.insert(i.first);
+            double temp=dfs(i.first,down,m,visited);
+            if(temp) return i.second*temp;
+        }
+    }
+    return 0;
+   }
+
+
+    vector<double> calcEquation(vector<vector<string>>& equations, vector<double>& values, vector<vector<string>>& queries) {
+        unordered_map<string,unordered_map<string,double>> m;
+        vector<double> ans;
+        for(int i=0;i<values.size();i++){
+            m[equations[i][0]].insert({equations[i][1],values[i]});
+            m[equations[i][1]].insert({equations[i][0],1/values[i]});
+        }
+        for(auto i:queries){
+            unordered_set<string> visited;
+            double temp=dfs(i[0],i[1],m,visited);
+            if(temp) ans.push_back(temp);
+            else ans.push_back(-1);
+        }
+        return ans;
+    }
+};
