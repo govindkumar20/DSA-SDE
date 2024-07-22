424fc3ab6ea5b9441bba4fa65697d4cb94581c86
Govind Kumar <120386156+govindkumar20@users.noreply.github.com>
2024-07-23 00:36:17 +0530

    Time: 280 ms (30.79%), Space: 201.6 MB (38.09%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,21 +1,19 @@
+// LCS of array and unique ascending elements of array gives LIS 
+
 class Solution {
 public:
-    int lengthOfLIS(vector<int>& nums) {
-        int len=nums.size();
-        vector<int> temp;
-        temp.push_back(nums[0]);
-        int length=1;
-        for(int i=1;i<len;i++)
-        {
-            if(temp.back()<nums[i])
-            {
-               temp.push_back(nums[i]); 
-               length++;
-            } else{
-                int ind=lower_bound(temp.begin(),temp.end(),nums[i])-temp.begin();
-                temp[ind]=nums[i];
+    int lengthOfLIS(vector<int>& nums1) {
+        int n=nums1.size();
+        set<int> s(nums1.begin(),nums1.end());  // for unique elements and ascending order
+        vector<int> nums2(s.begin(),s.end());
+        int m=nums2.size();
+        vector<vector<int>> dp(n+1,vector<int>(m+1,0));
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                if(nums1[i-1]==nums2[j-1]) dp[i][j]=1+dp[i-1][j-1];
+                else dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
             }
         }
-        return length;
+        return dp[n][m];
     }
 };
