28bf4d3568078ba9a190f9a619c3ec9e296d566e
Govind Kumar <120386156+govindkumar20@users.noreply.github.com>
2024-07-23 22:58:48 +0530

    Time: 4 ms (31.37%), Space: 9.6 MB (5.4%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,13 +1,17 @@
 class Solution {
-public:
+public: 
+    int solve(vector<int>& nums,vector<int>& dp,int n){
+        if(n<0) return 0;
+        if(n==0) return nums[0];
+        if(dp[n]!=-1) return dp[n]; //memoization
+        int take=nums[n]+solve(nums,dp,n-2);
+        int nottake=solve(nums,dp,n-1);
+        return dp[n]=max(take,nottake);
+    }
+    
     int rob(vector<int>& nums) {
-        if(nums.size()==1) return nums[0];
-      vector<int> dp(nums);
-      dp[1]=max(nums[0],nums[1]);
-      for(int i=2;i<nums.size();i++)
-      {
-        dp[i]=max(dp[i-1],nums[i]+dp[i-2]);
-      }
-      return dp.back();
+        int n=nums.size();
+        vector<int> dp(n+1,-1);
+        return solve(nums,dp,n-1);
     }
 };
