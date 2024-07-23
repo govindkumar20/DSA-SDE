36d410019bc9124dd5b013695f059a55f5f4ea8f
Govind Kumar <120386156+govindkumar20@users.noreply.github.com>
2024-07-23 21:00:25 +0530

    Time: 29 ms (89.84%), Space: 16.2 MB (16.75%) - LeetHub

--- 0673-number-of-longest-increasing-subsequence/0673-number-of-longest-increasing-subsequence.cpp
+++ 0673-number-of-longest-increasing-subsequence/0673-number-of-longest-increasing-subsequence.cpp
@@ -1,35 +1,30 @@
 class Solution {
 public:
     int findNumberOfLIS(vector<int>& nums) {
-        if(nums.size()==0) return 0;
-
-        vector<int> length(nums.size(),1);
-        vector<int> count(nums.size(),1);
-
-        int maxi=1;
-
-        for(int i=0;i<nums.size();i++)
-        {
-            for(int j=0;j<i;j++)
-            {
-                if(nums[j]<nums[i] && length[j]+1>length[i])
-                {
-                length[i]=length[j]+1;
-                count[i]=count[j];
-                } 
-                else if(nums[j]<nums[i] && length[j]+1==length[i])
-                {
-                 count[i]+=count[j];
+        int n=nums.size();
+        vector<int> len(n,1);
+        vector<int> cnt(n,1);
+        int maxlen=1;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<i;j++){
+                if(nums[i]>nums[j]){  //if its a same sewuence
+                    if(len[j]+1>len[i]){
+                        len[i]=len[j]+1;
+                        cnt[i]=cnt[j];
+                    } 
+                   else if(len[j]+1==len[i]){ // if its a different sequence
+                        cnt[i]+=cnt[j];
+                    }
                 }
             }
-            maxi=max(maxi,length[i]);
-
+            maxlen=max(maxlen,len[i]);
         }
         int result=0;
-        for(int i=0;i<nums.size();i++)
-        {
-            if(length[i]==maxi) result+=count[i];
+        for(int i=0;i<n;i++){
+            if(len[i]==maxlen){
+                result+=cnt[i];
+            }
         }
-        return result;
+        return  result;
     }
 };
