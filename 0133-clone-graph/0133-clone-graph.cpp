288f9f056427298354a437f6b114f3c222b3d052
Govind Kumar <120386156+govindkumar20@users.noreply.github.com>
2024-07-24 16:26:33 +0530

    Time: 3 ms (71.4%), Space: 11.9 MB (18.92%) - LeetHub

--- 0133-clone-graph/0133-clone-graph.cpp
+++ 0133-clone-graph/0133-clone-graph.cpp
@@ -1,44 +1,44 @@
-/*
-// Definition for a Node.
-class Node {
-public:
-    int val;
-    vector<Node*> neighbors;
-    Node() {
-        val = 0;
-        neighbors = vector<Node*>();
-    }
-    Node(int _val) {
-        val = _val;
-        neighbors = vector<Node*>();
-    }
-    Node(int _val, vector<Node*> _neighbors) {
-        val = _val;
-        neighbors = _neighbors;
-    }
-};
-*/
 
+/*
+
+  1----------2
+   | \       |
+   |   \     |
+   |     \   |
+   |       \ |    
+   4 --------3
+
+   map<Node*,Node*> 
+   1-1'
+   2-2'
+   3-3'
+   4-4'
+
+   1'-2'3'4'
+   2'-1'3'
+   3'-1'2'4'
+   4'-1'3'
+*/
 class Solution {
 public:
     Node* cloneGraph(Node* node) {
-        if(node==NULL) return NULL;
-        unordered_map<Node*,Node*> m;
-        Node* newNode=new Node(node->val,{});
+        if(node==NULL) return NULL; // if node is empty
+        unordered_map<Node*,Node*> vis;
         queue<Node*> q;
         q.push(node);
-        m[node]=newNode;
+        vis[node]=new Node(node->val);
+        
         while(!q.empty()){
-            Node* cur=q.front();
+            Node* curr=q.front();
             q.pop();
-            for( Node* i:cur->neighbors){
-                if(m.find(i)==m.end()){
-                    m[i]=new Node(i->val,{});
+            for(Node* i:curr->neighbors){
+                if(!vis[i]){
+                    vis[i]=new Node(i->val);
                     q.push(i);
                 }
-                m[cur]->neighbors.push_back(m[i]);
+                vis[curr]->neighbors.push_back(vis[i]);
             }
         }
-        return newNode;
+      return vis[node];
     }
 };
