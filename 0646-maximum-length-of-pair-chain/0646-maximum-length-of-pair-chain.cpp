144a117d7cd1586f98f0ea90c1aa1a9a890fbb0a
Govind Kumar <120386156+govindkumar20@users.noreply.github.com>
2024-07-25 12:21:31 +0530

    Time: 33 ms (90.9%), Space: 26.5 MB (49.23%) - LeetHub

--- 0646-maximum-length-of-pair-chain/0646-maximum-length-of-pair-chain.cpp
+++ 0646-maximum-length-of-pair-chain/0646-maximum-length-of-pair-chain.cpp
@@ -1,16 +1,21 @@
 class Solution {
- 
 public:
+    static bool cmp(vector<int>& a, vector<int>& b){
+        return a[1]<b[1];
+    }
+
+
     int findLongestChain(vector<vector<int>>& pairs) {
-       
-        sort(pairs.begin(),pairs.end()); // now left is not aur problem as it gets sorted
-        vector<int> lis;  // basic lis with slight modificationsf
-        for(int i=0;i<pairs.size();i++){
-            int ele=pairs[i][0];
-            auto ind=lower_bound(lis.begin(),lis.end(),ele);
-            if(ind==lis.end()) lis.push_back(pairs[i][1]);  // if theres no one greater than pair[0] insert pair[1] at last
-            else *ind=min(*ind,pairs[i][1]);  // if there is someone greater find the smallest one
+        sort(pairs.begin(),pairs.end(),cmp);
+        vector<int> temp;
+        temp.push_back(pairs[0][1]);
+        int cnt=1;
+        for(int i=1;i<pairs.size();i++){
+            if(pairs[i][0]>temp.back()){
+                cnt++;
+                temp.push_back(pairs[i][1]);
+            }
         }
-        return lis.size();
+        return cnt;
     }
 };
