ac746e527c62733625668d9f5f8e65be9cef2316
Govind Kumar <120386156+govindkumar20@users.noreply.github.com>
2024-07-27 02:20:17 +0530

    Time: 3 ms (36.72%), Space: 8.2 MB (23.33%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.cpp
+++ 0118-pascals-triangle/0118-pascals-triangle.cpp
@@ -1,15 +1,14 @@
 class Solution {
 public:
-    vector<vector<int>> generate(int numRows) {
+    vector<vector<int>> generate(int n) {
         vector<vector<int>> ans;
-        for(int i=0;i<numRows;i++)
-        {
-           vector<int> row(i+1,1); // it will handle 1st and last ones
-           for(int j=1;j<i;j++)
-           {
-               row[j]=ans[i-1][j]+ans[i-1][j-1];    // imp thing to know
-           }
-           ans.push_back(row);
+        ans.push_back({1});
+        for(int i=1;i<n;i++){
+            ans.push_back(vector<int> (i+1,1));
+            for(int j=1;j<i;j++){
+                ans[i][j]=ans[i-1][j]+ans[i-1][j-1];
+            }
+            
         }
         return ans;
     }
