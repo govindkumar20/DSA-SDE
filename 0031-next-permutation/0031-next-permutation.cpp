cb12afa99d0ce5ce44831ca301d67541c2c9abe5
Govind Kumar <120386156+govindkumar20@users.noreply.github.com>
2024-07-27 02:59:33 +0530

    Time: 0 ms (100%), Space: 14.8 MB (13.25%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -2,34 +2,20 @@ class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
         int n=nums.size();
-        int infpt=0;
-        for(int i=n-1;i>0;i--)    //find inflation point
-        {
-            if(nums[i]>nums[i-1])
-            {
-                infpt=i;
+        int ind=-1;
+        for( int i=n-2;i>=0;i--){
+            if(nums[i]<nums[i+1]){
+                ind=i;
                 break;
-            }
-        }
-        if(infpt==0)     // if no inflation point meansno next permutation
-        {
-            sort(nums.begin(),nums.end());
-            return;
+            } 
         }
        
-       //if there exist inflation point
-           
-            for(int i=n-1;i>=0;i--)
-            {
-                if(nums[i]>nums[infpt-1])
-                {
-                    swap(nums[i],nums[infpt-1]);
-                    sort(nums.begin()+infpt,nums.end());
-                break;
-                }
-                
+        for(int j=n-1;j>=ind && ind!=-1;j--){
+            if(nums[j]>nums[ind]) {
+                swap(nums[j],nums[ind]);
+            break;
             }
-        
-        
+        }
+        reverse(nums.begin()+ind+1,nums.end());
     }
 };
