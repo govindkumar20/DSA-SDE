7488dd893ff3d783e3462cde643ccbb321f8f117
Govind Kumar <120386156+govindkumar20@users.noreply.github.com>
2024-07-26 18:04:38 +0530

    Time: 1625 ms (36.05%), Space: 25.5 MB (27.96%) - LeetHub

--- 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.cpp
+++ 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.cpp
@@ -1,43 +1,45 @@
 class Solution {
-public:
-    int minCut(string s) {
-        int n=s.size();
-        vector<int> dp(n,-1);
-        return solve(0,s,n,dp)-1;
-    }
-
-    int solve(int i,string& s,int& n, vector<int> & dp)
-    {
-        if(i==n) return 0;
-        if(dp[i]!=-1) return dp[i];
-        int mincost=INT_MAX;
-        vector<char> temp;
-        for(int j=i;j<n;j++)
-        {
-            temp.push_back(s[j]);
-            if(ispalindrome(temp))
-            {
-                 int cost=1+solve(j+1,s,n,dp);
-                 mincost=min(mincost,cost);
-            }
-        }
-        return dp[i]= mincost;
-    }
-
-    bool ispalindrome(vector<char> & temp)
-    {
-
-        int i=0;
-        int j=temp.size()-1;
-        while(i<j)
-        {
-            if(temp[i]!=temp[j])
-            {
+public:  
+    int t[2001][2001];
+    bool isPalindrome(string &s, int i, int j){
+        while (i < j){
+            if (s[i] != s[j]){
                 return false;
             }
             i++;
             j--;
         }
+
         return true;
+    }
+
+    int solve(string &s, int i, int j){
+        if (i >= j){
+            return 0;
+        }
+
+        if (t[i][j] != -1){
+            return t[i][j];
+        }
+
+        if (isPalindrome(s, i, j) == true){
+            return 0;
+        }
+
+        int ans = INT_MAX;
+        for (int k = i; k < j; k++){
+            if (isPalindrome(s, i, k)){
+                ans = min(ans, 1 + solve(s, k + 1, j));
+            }
+        }
+
+        return t[i][j] = ans;
+    }
+
+    int minCut(string s){
+        int i = 0;
+        int j = s.length() - 1;
+        memset(t, -1, sizeof(t));
+        return solve(s, i, j);
     }
 };
