af31de3fd08bb6d6d2ed0f4e9af1438408b9750d
Govind Kumar <120386156+govindkumar20@users.noreply.github.com>
2024-07-29 17:35:36 +0530

    Time: 13 ms (69.57%), Space: 10.9 MB (72.31%) - LeetHub

--- 1228-minimum-cost-tree-from-leaf-values/1228-minimum-cost-tree-from-leaf-values.cpp
+++ 1228-minimum-cost-tree-from-leaf-values/1228-minimum-cost-tree-from-leaf-values.cpp
@@ -1,24 +1,23 @@
 class Solution {
 public:
-    int solve(vector<int>& arr, vector<vector<int>>& dp, int i, int j) {
-        if (i >= j) return 0;
-        if (dp[i][j] != -1) return dp[i][j];
+    int solve(vector<int>& arr, vector<vector<int>>& dp, int i, int j){
+        if(i>=j) return 0;
+        if(dp[i][j]!=-1) return dp[i][j];
+        int ans=INT_MAX;
+        for(int k=i;k<j;k++){
+            int sum=solve(arr,dp,i,k)+solve(arr, dp,k+1,j);
+            int a=*max_element(arr.begin()+i,arr.begin()+k+1);
+            int b=*max_element(arr.begin()+k+1,arr.begin()+j+1);
+             ans=min(ans,sum+a*b);
 
-        long ans = INT_MAX;
-
-        for (int k = i; k < j; ++k) {
-            long s = solve(arr, dp, i, k) + solve(arr, dp, k + 1, j);
-            int a = *max_element(arr.begin() + i, arr.begin() + k + 1); 
-            int b = *max_element(arr.begin() + k + 1, arr.begin() + j + 1); 
-            ans = min(ans, s + a * b);
         }
-
-        return dp[i][j] = static_cast<int>(ans); 
+        return dp[i][j]=ans;
     }
 
+ 
     int mctFromLeafValues(vector<int>& arr) {
-        int n = arr.size();
-        vector<vector<int>> dp(n, vector<int>(n, -1));
-        return solve(arr, dp, 0, n - 1);
+        int n=arr.size();
+        vector<vector<int>> dp(n,vector<int>(n,-1));
+        return solve(arr,dp,0,n-1);
     }
 };
