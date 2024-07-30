21c0f9c3ab89d4efb72de234f24c9402e2b0e324
Govind Kumar <120386156+govindkumar20@users.noreply.github.com>
2024-07-30 19:15:46 +0530

    Time: 9 ms (80.83%), Space: 13.1 MB (89.33%) - LeetHub

--- 0967-minimum-falling-path-sum/0967-minimum-falling-path-sum.cpp
+++ 0967-minimum-falling-path-sum/0967-minimum-falling-path-sum.cpp
@@ -1,35 +1,28 @@
+// just like triangle
+// build path from bottom to top
+
 class Solution {
 public:
     int minFallingPathSum(vector<vector<int>>& matrix) {
         int n=matrix.size();
-        int small=INT_MAX;
-        for(int i=n-2;i>=0;i--)
-        {
-            for(int j=0;j<n;j++)
-            {
-                if(j==0)
-                {
-             matrix[i][j]+=min(matrix[i+1][j],matrix[i+1][j+1]); 
-                } 
-                else if(j==n-1)
-                {
-         matrix[i][j]+=min(matrix[i+1][j-1],matrix[i+1][j]);
-                }
-                else{
-            matrix[i][j]+=min(matrix[i+1][j-1],min(matrix[i+1][j],matrix[i+1][j+1]));
-                }
-
+        for(int i=n-2;i>=0;i--){
+            for(int j=0;j<n;j++){
+                if(j-1<0) matrix[i][j]+=min(matrix[i+1][j],matrix[i+1][j+1]);
+                else if(j+1>n-1) matrix[i][j]+=min(matrix[i+1][j],matrix[i+1][j-1]);
+                else matrix[i][j]+=min(matrix[i+1][j-1],min(matrix[i+1][j],matrix[i+1][j+1]));
             }
         }
-
-        for(int j=0;j<n;j++)
-        {
-            if(matrix[0][j]<small)
-            {
-                small=matrix[0][j];
-            }
+        // for(int i=0;i<n;i++){
+        //     for(int j=0;j<n;j++){
+        //         cout<<matrix[i][j]<<" ";
+        //     }
+        //     cout<<endl;
+        // }
+        int ans=INT_MAX;
+        for(int j=0;j<n;j++){
+            ans=min(ans,matrix[0][j]);
         }
-        return small;
+
+        return ans;
     }
-
 };
