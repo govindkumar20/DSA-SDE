422d68b087b91ede85f0dfdd62fd718702e019e3
Govind Kumar <120386156+govindkumar20@users.noreply.github.com>
2024-08-01 19:57:49 +0530

    Time: 4 ms (60.56%), Space: 15.2 MB (14.16%) - LeetHub

--- 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.cpp
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.cpp
@@ -1,35 +1,24 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
+
 class Solution {
 public:
-    vector<vector<int>> ans;
     vector<vector<int>> levelOrder(TreeNode* root) {
-        if(root==NULL) return ans;
-        queue<TreeNode*> q;
-        q.push(root);
-        while(!q.empty()){ //jb tk queue me kuch hai
-            int n=q.size();
-             vector<int> level;
-            for(int i=0;i<n;i++){
-                TreeNode* node=q.front();
-                q.pop();
-               
-                if(node->left) q.push(node->left);  // if left child or subtree exists
-                if(node->right) q.push(node->right); // if right child of subtree exists
-                level.push_back(node->val); // val ko levels me push back krva do
 
-            }
-            ans.push_back(level);
+        vector<vector<int>> ans;
+        if(root==NULL) return ans;
+       queue<TreeNode*> q;
+       q.push(root);
+       while(!q.empty()){
+        int n=q.size();
+        vector<int> level;
+        for(int i=0;i<n;i++){
+            TreeNode* node=q.front();
+            q.pop();
+            if(node->left) q.push(node->left);
+            if(node->right) q.push(node->right);
+            level.push_back(node->val);
         }
-        return ans;
+        ans.push_back(level);
+       }
+       return ans;
     }
 };
