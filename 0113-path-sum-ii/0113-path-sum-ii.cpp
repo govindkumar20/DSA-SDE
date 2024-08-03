3bcafd720a2428b6d0d5f1c51c817c5bb5781ea8
Govind Kumar <120386156+govindkumar20@users.noreply.github.com>
2024-08-03 17:23:01 +0530

    Time: 23 ms (5.36%), Space: 38.4 MB (6.13%) - LeetHub

--- 0113-path-sum-ii/0113-path-sum-ii.cpp
+++ 0113-path-sum-ii/0113-path-sum-ii.cpp
@@ -1,38 +1,31 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
+
 class Solution {
 public:
-    vector<vector<int>> pathSum(TreeNode* root, int targetSum) {
-     vector<int> v;
-     vector<vector<int>> ans;
-     dfs(root,targetSum,ans,v);
-     return ans;
+   void dfs(TreeNode* root, vector<vector<int>>& ans, vector<int> cur, int targetSum,int curr_sum){
+    if(root==NULL) return;
+    if(root->left || root->right) {
+        cur.push_back(root->val);
+        curr_sum+=root->val;
     }
-    void dfs(TreeNode* root,int targetSum,vector<vector<int>>& ans, vector<int>& v){
-       if(root==NULL) return;
-        v.push_back(root->val);
-        if(root->left==NULL && root->right==NULL){
-            int sum=0;
-            for(auto i:v) sum+=i;
-            if(sum==targetSum){
-                ans.push_back(v);
-            }
-         
+    else{
+        cur.push_back(root->val);
+        curr_sum+=root->val;
+        if(targetSum==curr_sum){
+            ans.push_back(cur);
         }
-        dfs(root->left,targetSum,ans,v);
-        dfs(root->right,targetSum,ans,v);
-        v.pop_back();
-       //for(auto i:v) cout<<i<<" ";
 
-       
+    }
+    dfs(root->left,ans,cur,targetSum,curr_sum);
+    dfs(root->right,ans,cur,targetSum,curr_sum);
+
+   }
+
+
+    vector<vector<int>> pathSum(TreeNode* root, int targetSum) {
+        vector<vector<int>> ans;
+        vector<int> cur;
+        int curr_sum=0;
+        dfs(root,ans,cur,targetSum,curr_sum);
+        return ans;
     }
 };
