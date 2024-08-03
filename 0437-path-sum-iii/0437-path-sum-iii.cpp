55fb6f9f34dc0d16ba8db6b849a5253e2381c3cb
Govind Kumar <120386156+govindkumar20@users.noreply.github.com>
2024-08-03 18:17:03 +0530

    Time: 20 ms (42.39%), Space: 17.8 MB (60.48%) - LeetHub

--- 0437-path-sum-iii/0437-path-sum-iii.cpp
+++ 0437-path-sum-iii/0437-path-sum-iii.cpp
@@ -1,25 +1,20 @@
 
 class Solution {
 public:
-  long long count=0;
-   long long sum=0;
-    void dfs(TreeNode* root,long long targetSum,long long& count){
+    long long count=0;
+    long long sum=0;
+    void dfs(TreeNode* root, long long targetSum){
         if(root==NULL) return;
-         sum+=root->val;
-             if(root->val==targetSum){
-                count++;
-             } 
-             dfs(root->left,targetSum-root->val,count);
-             dfs(root->right,targetSum-root->val,count);
+        //sum+=root->val;
+        if(root->val==targetSum) count++;
+        dfs(root->left,targetSum-root->val);
+        dfs(root->right,targetSum-root->val);
     }
-
- 
-    long long pathSum(TreeNode* root, long long targetSum) {
+    int pathSum(TreeNode* root, int targetSum) {
         if(root==NULL) return 0;
-      
-        dfs(root,targetSum,count);
-         pathSum(root->left,targetSum);
-         pathSum(root->right,targetSum);
-         return count;
+        dfs(root,targetSum);
+        pathSum(root->left,targetSum);
+        pathSum(root->right,targetSum);
+        return count;
     }
 };
