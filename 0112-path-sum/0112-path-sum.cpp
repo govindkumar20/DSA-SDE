79a4911fb28499260b87a58528cb223c4af29cd1
Govind Kumar <120386156+govindkumar20@users.noreply.github.com>
2024-08-03 17:16:04 +0530

    Time: 11 ms (23.5%), Space: 20.1 MB (18.8%) - LeetHub

--- 0112-path-sum/0112-path-sum.cpp
+++ 0112-path-sum/0112-path-sum.cpp
@@ -1,25 +1,38 @@
-
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
-        int sum=0;
-        bool flag=0;
-        void dfs(TreeNode* root,int targetSum){
-            if(root==NULL) return ;
-            int prev=sum; // to save initial sum for backtracking
-            if(root->left || root->right){
-                sum+=root->val;
-            } else{
-               sum+=root->val;
-               if(sum==targetSum) flag=1;
-            }
-            dfs(root->left,targetSum);
-            dfs(root->right,targetSum);
-            sum=prev; // for backtracking
+    int flag=0;
+    void dfs(TreeNode* root,int curr_sum,int targetSum){
+        if(root==NULL) return;
+        if(root->left || root->right){
+            curr_sum+=root->val;
         }
-    bool hasPathSum(TreeNode* root, int targetSum) {
-       dfs(root,targetSum);
-       return flag;
+        else {
+            curr_sum+=root->val;
+            if(curr_sum==targetSum) {
+                flag=1;
+                
+            }
+        }
+        dfs(root->left,curr_sum,targetSum);
+        dfs(root->right,curr_sum,targetSum);
+    }
 
+    bool hasPathSum(TreeNode* root, int targetSum) {
+        int curr_sum=0;
+        dfs(root,curr_sum,targetSum);
+        if(flag==1) return true;
+        else return false;
         
     }
 };
