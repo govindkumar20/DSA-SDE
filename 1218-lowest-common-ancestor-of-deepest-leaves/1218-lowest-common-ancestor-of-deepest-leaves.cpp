4c402310df4b998e66c5fa9cc4fc16dbb3f50b2d
Govind Kumar <120386156+govindkumar20@users.noreply.github.com>
2024-08-04 10:51:00 +0530

    Time: 16 ms (15.73%), Space: 21.6 MB (22.31%) - LeetHub

--- 1218-lowest-common-ancestor-of-deepest-leaves/1218-lowest-common-ancestor-of-deepest-leaves.cpp
+++ 1218-lowest-common-ancestor-of-deepest-leaves/1218-lowest-common-ancestor-of-deepest-leaves.cpp
@@ -1,28 +1,41 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
-class Solution {
-public:  int d=0;
-    int depth(TreeNode* root){ // depth calculator func
-        if(root==NULL) return 0;
-        return max(depth(root->left),depth(root->right))+1;
-    }
-    TreeNode* lcaDeepestLeaves(TreeNode* root) {
-        if(root==NULL) return NULL;
 
-        int left=depth(root->left); // left subtree depth
-        int right=depth(root->right); // right subtree depth
-        
-        if(left==right) return root; // if both equal means root is lca
-        else if(left>right) return lcaDeepestLeaves(root->left); // bcz we want to reach deepest leaves so choose longer depth
-        else return lcaDeepestLeaves(root->right);
+class Solution {
+public:
+  
+  vector<TreeNode*> levelOrder(TreeNode* root){
+     vector<TreeNode*> level;
+     if(root==NULL) return level;
+     queue<TreeNode*> q;
+     q.push(root);
+     while(!q.empty()){
+        int n=q.size();
+       level.clear();
+        for(int i=0;i<n;i++){
+            TreeNode* node=q.front();
+            q.pop();
+            if(node->left) q.push(node->left);
+            if(node->right) q.push(node->right);
+            level.push_back(node);
+        }
+       
+     }
+     return level;
+   }
+
+   TreeNode* lca(TreeNode* root, TreeNode* p, TreeNode* q){
+    if(root==NULL) return 0;
+    if(root==p || root==q) return root;
+    TreeNode* left=lca(root->left,p,q);
+    TreeNode* right=  lca(root->right,p,q);
+    if(left && right) return root;
+    else if(left) return left;
+    else return right;
+   }
+
+
+    TreeNode* lcaDeepestLeaves(TreeNode* root) {
+    vector<TreeNode*> level=levelOrder(root);
+    if(level.size()==1) return lca(root,level[0],NULL);
+    else return lca(root,level[0],level[level.size()-1]);
     }
 };
