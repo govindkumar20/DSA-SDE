3e39fc6a6ac7ade51832a529479bf0acc127a5e7
Govind Kumar <120386156+govindkumar20@users.noreply.github.com>
2024-08-04 19:35:56 +0530

    Time: 33 ms (5.55%), Space: 30.2 MB (5.04%) - LeetHub

---
+++ 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.cpp
@@ -0,0 +1,21 @@
+
+class Solution {
+public:
+    int countNodes(TreeNode* root) {
+        if(root==NULL) return 0;
+        int count=0;
+        queue<TreeNode*> q;
+        q.push(root);
+        while(!q.empty()){
+            int n=q.size();
+            for(int i=0;i<n;i++){
+                count++;
+                TreeNode* node=q.front();
+                q.pop();
+                if(node->left) q.push(node->left);
+                if(node->right) q.push(node->right);
+            }
+        }
+        return count;
+    }
+};
