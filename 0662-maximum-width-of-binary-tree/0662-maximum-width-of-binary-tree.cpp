e53b597e4626d49183c655cd27c5ad0069af4fee
Govind Kumar <120386156+govindkumar20@users.noreply.github.com>
2024-08-05 19:18:00 +0530

    Time: 4 ms (85.45%), Space: 18.6 MB (11.26%) - LeetHub

--- 0662-maximum-width-of-binary-tree/0662-maximum-width-of-binary-tree.cpp
+++ 0662-maximum-width-of-binary-tree/0662-maximum-width-of-binary-tree.cpp
@@ -1,36 +1,22 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
+
 class Solution {
 public:
     int widthOfBinaryTree(TreeNode* root) {
         queue<pair<TreeNode*,int>> q;
-        q.push({root,0});
-        long ans=0;
-        while(!q.empty())
-        {
-            int n=q.size();
-            long start=q.front().second;
-            long end=q.back().second;
+        q.push({root,0}); // node,index
+        long long ans=0;
+        while(!q.empty()){
+            long long start=q.front().second;
+            long long end=q.back().second;
             ans=max(ans,end-start+1);
-            for(int i=0;i<n;i++)
-            {
-                pair<TreeNode*,int> p=q.front();
-                TreeNode* node=p.first;
-                int index=p.second;
+            long n=q.size();
+            for(int i=0;i<n;i++){
+                TreeNode* node=q.front().first;
+                int index=q.front().second;
                 q.pop();
                 if(node->left) q.push({node->left,(long long)2*index+1});
                 if(node->right) q.push({node->right,(long long)2*index+2});
             }
-
         }
         return ans;
     }
