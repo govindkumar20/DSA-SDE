70f49639c6bfa5f9ce3f15d5c54b566c8b437c94
Govind Kumar <120386156+govindkumar20@users.noreply.github.com>
2024-08-06 12:34:49 +0530

    Time: 8 ms (19.1%), Space: 15.5 MB (26.89%) - LeetHub

--- 0896-smallest-subtree-with-all-the-deepest-nodes/0896-smallest-subtree-with-all-the-deepest-nodes.cpp
+++ 0896-smallest-subtree-with-all-the-deepest-nodes/0896-smallest-subtree-with-all-the-deepest-nodes.cpp
@@ -1,15 +1,39 @@
 
-class Solution {  
-public: int dfs(TreeNode* root){
-    if(root==NULL) return 0;
-    return max(dfs(root->left),dfs(root->right))+1;
-}
-    TreeNode* subtreeWithAllDeepest(TreeNode* root) {
-        int left=dfs(root->left);
-        int right=dfs(root->right);
-        if(left==right) return root;
-        else if(left>right) return subtreeWithAllDeepest(root->left);
-        else return subtreeWithAllDeepest(root->right);
+class Solution {
+public:
+   
+    vector<TreeNode*> levelOrder(TreeNode* root){ 
+        vector<TreeNode*> lvl;
+        if(root==NULL) return lvl;
+        queue<TreeNode*> q;
+        q.push(root);
+        while(!q.empty()){
+            int n=q.size();
+            lvl.clear();
+            for(int i=0;i<n;i++){
+                TreeNode* node=q.front();
+                q.pop();
+                if(node->left) q.push(node->left);
+                if(node->right) q.push(node->right);
+                lvl.push_back(node);
+            }  
+        }
+        return lvl;
+    }
 
+    TreeNode* lca(TreeNode* root, TreeNode* p, TreeNode* q){
+        if(root==NULL) return 0;
+        if(root==p || root==q) return root;
+        TreeNode* left=lca(root->left,p,q);
+        TreeNode* right=lca(root->right,p,q);
+        if(left && right) return root;
+        else if(left) return left;
+        else return right;
+    }
+
+    TreeNode* subtreeWithAllDeepest(TreeNode* root) {
+        vector<TreeNode*> level=levelOrder(root);
+        if(level.size()==1) return lca(root,level[0],NULL);
+        else return lca(root,level[0],level[level.size()-1]);
     }
 };
