f2d0deca26469f76faa63e60e57f561d4e890cdc
Govind Kumar <120386156+govindkumar20@users.noreply.github.com>
2024-08-06 14:11:03 +0530

    Time: 3 ms (83.71%), Space: 16.4 MB (7.02%) - LeetHub

---
+++ 0893-all-nodes-distance-k-in-binary-tree/0893-all-nodes-distance-k-in-binary-tree.cpp
@@ -0,0 +1,37 @@
+
+class Solution {
+public:
+vector<int> ans;
+    //making graph
+    void makeGraph(TreeNode* root,vector<vector<int>>& graph){
+        if(root->left){
+            makeGraph(root->left,graph);
+            graph[root->val].push_back(root->left->val);
+            graph[root->left->val].push_back(root->val);
+        }
+        if(root->right){
+            makeGraph(root->right,graph);
+            graph[root->val].push_back(root->right->val);
+            graph[root->right->val].push_back(root->val);
+        }
+    }
+
+    void findNodes(vector<vector<int>>& graph,int node,int k, int distance,int parent){
+        if(distance==k){
+            ans.push_back(node);
+        }
+        for(auto i:graph[node]){
+            if(i!=parent){
+            findNodes(graph,i,k,distance+1,node);
+            }
+        }
+    }
+    
+
+    vector<int> distanceK(TreeNode* root, TreeNode* target, int k) {
+        vector<vector<int>> graph(501);
+        makeGraph(root,graph);
+        findNodes(graph,target->val,k,0,-1);
+        return ans;
+    }
+};
