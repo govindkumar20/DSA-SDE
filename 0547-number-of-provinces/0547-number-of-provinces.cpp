eb7b6cb4d477f640f0da9c698440dab80f130163
Govind Kumar <120386156+govindkumar20@users.noreply.github.com>
2024-08-21 17:35:55 +0530

    Time: 69 ms (5.03%), Space: 66.7 MB (5.01%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -1,35 +1,39 @@
 class Solution {
- public:
-     void dfs(vector<vector<int>>& matrix,vector<int>& vis,vector<vector<int>> adj,int node){
+public:
+    void dfs(int node, vector<int>& vis, vector<vector<int>> adj,vector<vector<int>>& isConnected){
         vis[node]=1;
         for(auto i:adj[node]){
-            if(!vis[i]) dfs(matrix,vis,adj,i);
+            if(!vis[i]) {
+                
+                dfs(i,vis,adj,isConnected);
+            }
         }
-     }
+    }
 
-    int findCircleNum(vector<vector<int>>& matrix) {
-        int n=matrix.size();
-        int m=matrix[0].size();
-
+    int findCircleNum(vector<vector<int>>& isConnected) {
+        int n=isConnected.size();
+         
+        //making adj list
         vector<vector<int>> adj(n);
         for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(matrix[i][j]==1){
+            for(int j=0;j<n;j++){
+                if(isConnected[i][j]==1){
                     adj[i].push_back(j);
                     adj[j].push_back(i);
                 }
             }
         }
 
-        int count=0;
+        //visited array
         vector<int> vis(n,0);
+        int provinces=0;
         for(int i=0;i<n;i++){
-            if(!vis[i]) {
-                count++;
-                dfs(matrix,vis,adj,i);
-            } 
-        }
+            if(!vis[i]){
+                dfs(i,vis,adj,isConnected);
+                provinces++;
 
-        return count;
+            }
+        }
+        return provinces;
     }
 };
