1da0aa9408f33bee45e291c54ef1cbc8cc7857ac
Govind Kumar <120386156+govindkumar20@users.noreply.github.com>
2024-08-22 02:23:10 +0530

    Time: 6 ms (62.69%), Space: 16.9 MB (56.43%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -1,22 +1,24 @@
 class Solution {
 public:
-    void dfs(vector<vector<int>>& image, int i,int j,int color,int starting_color){
+    vector<int> xm={-1,0,1,0};
+    vector<int> ym={0,1,0,-1};
+    void dfs(int i, int j, vector<vector<int>>& image,int startingColor, int color){
         int n=image.size();
         int m=image[0].size();
-        if(i<0 || j<0 || i==n || j==m || image[i][j]==color|| image[i][j]!=starting_color) return;
-        image[i][j]=color;
-       dfs(image,i-1,j,color,starting_color);
-       dfs(image,i+1,j,color,starting_color);
-       dfs(image,i,j-1,color,starting_color);
-        dfs(image,i,j+1,color,starting_color);
-    }
-   
-    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
-        // simple dfs hai , basically neighbours updation
-        int starting_color=image[sr][sc];
-        if (starting_color != color) {
-            dfs(image, sr, sc, color, starting_color);
+        if(i>=0 && i<=n-1 && j>=0 && j<=m-1 && image[i][j]==startingColor && image[i][j]!=color) {
+            image[i][j]=color;
+            for(int p=0;p<4;p++){
+                dfs(i+xm[p],j+ym[p],image,startingColor,color);
+            }
         }
+    }
+
+
+    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
+        int n=image.size();
+        int m=image[0].size();
+        int startingColor=image[sr][sc];
+        dfs(sr,sc,image,startingColor,color);
         return image;
     }
 };
