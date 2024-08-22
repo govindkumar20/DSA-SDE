07e52f6f644c19dc679911f3cdf4f81fd6babf7b
Govind Kumar <120386156+govindkumar20@users.noreply.github.com>
2024-08-22 23:35:48 +0530

    Time: 9 ms (70.66%), Space: 14.2 MB (35.94%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.cpp
+++ 0130-surrounded-regions/0130-surrounded-regions.cpp
@@ -2,15 +2,16 @@ class Solution {
 public:
    vector<int> xm={-1,0,1,0};
    vector<int> ym={0,1,0,-1};
-   void dfs(int i, int j, vector<vector<int>>& vis, vector<vector<char>>& board){
-    int n=board.size();
-    int m=board[0].size();
-    if(i>=0 || i<n || j>=0 || j<m || board[i][j]!='O' || vis[i][j]) return;
-    vis[i][j]=1;
-    for(int p=0;p<4;p++){
-    dfs(i+xm[p],j+ym[p],vis,board);
+   void dfs(int i, int j, vector<vector<int>>& vis, vector<vector<char>>& board) {
+    int n = board.size();
+    int m = board[0].size();
+    if (i < 0 || i >= n || j < 0 || j >= m || board[i][j] != 'O' || vis[i][j] == 1) return;
+    vis[i][j] = 1;
+    for (int p = 0; p < 4; p++) {
+        dfs(i + xm[p], j + ym[p], vis, board);
     }
-   }
+}
+
 
     void solve(vector<vector<char>>& board) {
         int n=board.size();
@@ -30,8 +31,8 @@ public:
             if(board[i][m-1]=='O' && vis[i][m-1]==0)  dfs(i,m-1,vis,board);
         }
 
-        for(int i=1;i<n-1;i++){
-            for(int j=1;j<m-1;j++){
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
                 if(!vis[i][j] && board[i][j]=='O') board[i][j]='X';
             }
           
