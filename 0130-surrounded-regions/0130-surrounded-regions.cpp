244d4974b4e71662b54feea1b6af8585207bc8f8
Govind Kumar <120386156+govindkumar20@users.noreply.github.com>
2024-08-22 23:09:05 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.cpp
+++ 0130-surrounded-regions/0130-surrounded-regions.cpp
@@ -1,41 +1,42 @@
 class Solution {
 public:
-    vector<int> xm={-1,0,1,0}; // coordinates of 4 drn's
-    vector<int> ym={0,1,0,-1};
-    void dfs(vector<vector<char>>& board, vector<vector<int>>& vis, int i, int j){  // simple old dfs
-        int n=board.size();
-        int m=board[0].size();
-        if(i>=0 && j>=0 && i<n && j<m && vis[i][j]==0  && board[i][j]=='O'){
-            vis[i][j]=1;
-            for(int p=0;p<4;p++){
-                dfs(board,vis,i+xm[p],j+ym[p]);
-            }
-        }
+   vector<int> xm={-1,0,1,0};
+   vector<int> ym={0,1,0,-1};
+   void dfs(int i, int j, vector<vector<int>>& vis, vector<vector<char>>& board){
+    int n=board.size();
+    int m=board[0].size();
+    if(i>=0 || i<n || j>=0 || j<m || board[i][j]!='O' || vis[i][j]) return;
+    vis[i][j]=1;
+    for(int p=0;p<4;p++){
+    dfs(i+xm[p],j+ym[p],vis,board);
     }
+   }
+
     void solve(vector<vector<char>>& board) {
         int n=board.size();
         int m=board[0].size();
-        vector<vector<int>> vis(n,vector<int>(m,0)); // visited array
 
-           // traverse each boundary and if there exist O , apply dfs and we cannot change this cell and its  connected cells
-        for(int j=0;j<m;j++){ 
-            if(board[0][j]=='O' && vis[0][j]==0)  dfs(board,vis,0,j);  
-            if(board[n-1][j]=='O' && vis[n-1][j]==0)  dfs(board,vis,n-1,j);
+        vector<vector<int>> vis(n,vector<int>(m,0));
+        for(int j=0;j<m;j++) {
+            if(board[0][j]=='O') dfs(0,j,vis,board);
+        }
+        for(int j=0;j<m;j++) {
+            if(board[n-1][j]=='O') dfs(n-1,j,vis,board);
+        }
+        for(int i=1;i<n-1;i++){
+              if(board[i][0]=='O') dfs(i,0,vis,board);
+        }
+        for(int i=1;i<n-1;i++) {
+            if(board[i][m-1]=='O')  dfs(i,m-1,vis,board);
         }
 
-        for(int i=0;i<n;i++){
-            if(board[i][0]=='O' && vis[i][0]==0) dfs(board,vis,i,0);
-            if(board[i][m-1]=='O' && vis[i][m-1]==0)  dfs(board,vis,i,m-1);
-        }
-
-        for(int i=0;i<n;i++){    // if there exist a surrounding regin, make it X
-            for(int j=0;j<m;j++){
-                if(board[i][j]=='O' && vis[i][j]==0){
-                    board[i][j]='X';
-                }
+        for(int i=1;i<n-1;i++){
+            for(int j=1;j<m-1;j++){
+                if(!vis[i][j] && board[i][j]=='O') board[i][j]='X';
             }
+          
         }
-
+        
 
     }
-};   
+};
