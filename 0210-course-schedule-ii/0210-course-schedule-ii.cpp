a13bf043f00c7d8c861ed7e1ae1c839e9296d681
Govind Kumar <120386156+govindkumar20@users.noreply.github.com>
2024-08-25 10:44:19 +0530

    Time: 21 ms (18.75%), Space: 18.1 MB (28.28%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.cpp
+++ 0210-course-schedule-ii/0210-course-schedule-ii.cpp
@@ -1,30 +1,30 @@
 class Solution {
 public:
     vector<int> findOrder(int n, vector<vector<int>>& prerequisites) {
-        vector<int> indegree(n,0);
-        vector<int> v;
-        unordered_map<int,vector<int>> m;
+        vector<vector<int>> adj(n);
+        vector<int> indegree(n);
+        vector<int> topo;
         for(auto i:prerequisites){
-            m[i[1]].push_back(i[0]);
+            adj[i[1]].push_back(i[0]);
             indegree[i[0]]++;
         }
-
         queue<int> q;
         int processed=0;
         for(int i=0;i<n;i++){
             if(indegree[i]==0) q.push(i);
         }
         while(!q.empty()){
-            int temp=q.front();
-            v.push_back(temp);
+            int node=q.front();
+            topo.push_back(node);
             q.pop();
             processed++;
-            for(auto i:m[temp]){
+            for(auto i:adj[node]){
                 indegree[i]--;
                 if(indegree[i]==0) q.push(i);
+
             }
         }
-        if(processed==n) return v;
-        else return {};
+        if(processed==n) return topo;
+        return {};
     }
 };
