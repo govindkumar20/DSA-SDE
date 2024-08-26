190b971c1db9c0a32042a2fc6bc23f8d1ca39eac
Govind Kumar <120386156+govindkumar20@users.noreply.github.com>
2024-08-26 21:14:05 +0530

    Time: 23 ms (14.1%), Space: 17.7 MB (47.36%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -1,47 +1,28 @@
 class Solution {
 public:
     bool canFinish(int n, vector<vector<int>>& prerequisites) {
-        // making adj list and indegree vector
-        vector<int> indegree(n,0);
-        unordered_map<int,vector<int>> adj;
-        for(auto &pair:prerequisites){
-            adj[pair[1]].push_back(pair[0]);
-            indegree[pair[0]]++;
+        vector<vector<int>> adj(n);
+        vector<int> indegree(n);
+        for(auto i:prerequisites){
+            adj[i[1]].push_back(i[0]);
+            indegree[i[0]]++;
         }
-
         queue<int> q;
-
-        // push node with indegree 0 as it doesnt depent on anyone
+        int processed=0;
         for(int i=0;i<n;i++){
-            if(indegree[i]==0){
-                q.push(i);
-            }
+            if(indegree[i]==0) q.push(i);
         }
 
-        int processed=0;
-        // BFS
         while(!q.empty()){
             int node=q.front();
             q.pop();
             processed++;
-            for(auto i:adj[node]){  
-                indegree[i]--;   // as node vanishes, remove its dependencies from others thats why indegree--;
-                if(indegree[i]==0) q.push(i); // when node become independent i.e indegree==0 insert it into queue
+            for(auto i:adj[node]){
+                indegree[i]--;
+                if(indegree[i]==0) q.push(i);
             }
         }
-        return processed==n;
+        if(processed==n) return true;
+        return false;
     }
-    
 };
-
-/*
-
-    ex:-           0                   // independent
-          (-1)  /    \   (-1)
-               1       2
-               |       |    
-               3       4
-                 \    /
-                    5 
-
-*/
