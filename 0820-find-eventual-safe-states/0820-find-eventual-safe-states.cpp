a1203789525c0a3b06ab00441eebf1ef40a678de
Govind Kumar <120386156+govindkumar20@users.noreply.github.com>
2024-08-26 23:09:25 +0530

    Time: 147 ms (33.23%), Space: 65.9 MB (11.3%) - LeetHub

--- 0820-find-eventual-safe-states/0820-find-eventual-safe-states.cpp
+++ 0820-find-eventual-safe-states/0820-find-eventual-safe-states.cpp
@@ -1,33 +1,32 @@
 class Solution {
 public:
     vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
-        int V=graph.size();
-        vector<int> outdegree(V,0);
-        vector<int> safe(V);
-        vector<int> ans;
-        vector<vector<int>> l(V);
-         queue<int> q;
-        for(int i=0;i<V;i++){
-            for(auto val:graph[i]){   // male a vector list jisme node and uske upar incoming edges nodes ho 
-                l[val].push_back(i);   // kyunki hame subtract krni hogi outdegree after deleting dafe nodes
-            }
-                outdegree[i]=graph[i].size();
-                if(outdegree[i]==0)  q.push(i);
-            
+    int n=graph.size();
+    vector<int> safeNodes(n,0);
+    queue<int> q;
+    vector<vector<int>> adj(n);
+    vector<int> outdegree(n);
+    for(int i=0;i<n;i++){
+        for(int j=0;j<graph[i].size();j++){
+            adj[graph[i][j]].push_back(i);
         }
-       
-        while(!q.empty()){
-            int node=q.front();
-            q.pop();
-            safe[node]=1;  // keeping safenodes in memory
-            for(auto i:l[node]){
-                outdegree[i]--; // outdegree-- after deleting safe node
-                if(outdegree[i]==0) q.push(i);
-            }
+        outdegree[i]=graph[i].size();
+        if(outdegree[i]==0) q.push(i);
+    }
+
+    while(!q.empty()){
+        int node=q.front();
+        q.pop();
+        safeNodes[node]=1;
+        for(auto i:adj[node]){
+            outdegree[i]--;
+            if(outdegree[i]==0) q.push(i);
         }
-        for(int i=0;i<V;i++){
-            if(safe[i]) ans.push_back(i);
-        }
-        return ans;
+    }
+    vector<int> ans;
+    for(int i=0;i<n;i++){
+        if(safeNodes[i]==1) ans.push_back(i);
+    }
+     return ans;
     }
 };
