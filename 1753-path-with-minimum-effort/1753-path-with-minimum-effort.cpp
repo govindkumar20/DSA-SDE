1b205a155cb4549938e937f4d9617ee3a98024c6
Govind Kumar <120386156+govindkumar20@users.noreply.github.com>
2024-08-27 16:51:58 +0530

    Time: 89 ms (56.59%), Space: 23.9 MB (68.49%) - LeetHub

--- 1753-path-with-minimum-effort/1753-path-with-minimum-effort.cpp
+++ 1753-path-with-minimum-effort/1753-path-with-minimum-effort.cpp
@@ -1,14 +1,14 @@
-#define pair pair<int, pair<int,int>>
-
+#define pair pair<int,pair<int,int>>
 class Solution {
-public: vector<int> xm={-1,0,1,0}; // 4 drn for travelling
-        vector<int> ym={0,1,0,-1};
+public:
+    vector<int> xm={-1,0,1,0};
+    vector<int> ym={0,1,0,-1};
     int minimumEffortPath(vector<vector<int>>& heights) {
         int n=heights.size();
         int m=heights[0].size();
-        vector<vector<int>> vis(n,vector<int>(m,1e9));
-        priority_queue<pair,vector<pair>,greater<pair>> q; // min_heap bcz we want min effort to stay at top
-        vis[0][0]=0; // initial constraints
+        vector<vector<int>> dist(n,vector<int>(m,1e9));
+        priority_queue<pair,vector<pair>,greater<pair>> q;
+        dist[0][0]=0;
         q.push({0,{0,0}});
         while(!q.empty()){
             int r=q.top().second.first;
@@ -19,16 +19,15 @@ public: vector<int> xm={-1,0,1,0}; // 4 drn for travelling
             for(int i=0;i<4;i++){
                 int nr=r+xm[i];
                 int nc=c+ym[i];
-                if(nr>=0 && nr<n && nc>=0 && nc<m ){
-                    int nd=max(d,abs(heights[nr][nc]-heights[r][c])); // we want max absolute dist between adj cells
-                    if(nd<vis[nr][nc]){  // if that dist is less then present val then present value = that dist
-                        vis[nr][nc]=nd; 
-                        q.push({nd,{nr,nc}}); // push that in q
-                    }
+                if(nr>=0 && nc>=0 && nr<n && nc<m){
+                    int effort=max(d,abs(heights[nr][nc]-heights[r][c]));
+                    if(effort<dist[nr][nc]) {
+                    dist[nr][nc]=effort;
+                    
+                    q.push({effort,{nr,nc}});
+                }
                 }
             }
-
-
         }
         return 0;
     }
