6e2278f334bb95d0b9629c2f13d0d049fcc3b711
Govind Kumar <120386156+govindkumar20@users.noreply.github.com>
2024-08-27 20:57:14 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0803-cheapest-flights-within-k-stops/0803-cheapest-flights-within-k-stops.cpp
+++ 0803-cheapest-flights-within-k-stops/0803-cheapest-flights-within-k-stops.cpp
@@ -2,28 +2,29 @@ class Solution {
 public:
     int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
         vector<pair<int,int>> adj[n];
-        for(auto it:flights){  //adj list
-            adj[it[0]].push_back({it[1],it[2]});
+        for(auto i:flights){
+            adj[i[0]].push_back({i[1],i[2]});
         }
         vector<int> dist(n,1e9);
-        queue<pair<int,pair<int,int>>> q; // {stops,{node,cost}}
+        queue<pair<int,pair<int,int>>> q; // stops,{node,cost}
+        dist[0]=0;
         q.push({0,{src,0}});
-        dist[src]=0;
         while(!q.empty()){
-           int  stops=q.front().first;
-           int node=q.front().second.first;
-           int cost=q.front().second.second;
-         q.pop();
-           for(auto itr:adj[node]){
-            int edjW=itr.second;
-            int adjnode=itr.first;
-            if(cost+edjW<dist[adjnode] && stops<=k){
-                dist[adjnode]=cost+edjW;
-                q.push({stops+1,{adjnode,cost+edjW}});
+            int node=q.front().second.first;
+            int cost=q.front().second.second;
+            int steps=q.front().first;
+            q.pop();
+            for(auto i:adj[node]){
+                int adjNode=i.first;
+                int edgW=i.second;
+                if(cost+edgW<dist[adjNode] && steps<=k){
+                    dist[adjNode]=cost+edgW;
+                    q.push({steps+1,{adjNode,dist[adjNode]}});
+                }
             }
-           }
-
         }
-       return dist[dst]==1e9?-1:dist[dst];
+        return dist[dst]==1e9?-1:dist[dst];
+
+
     }
 };
