d8d3335edcfbfe51444f8ac4d790b4adf1f1c945
Govind Kumar <120386156+govindkumar20@users.noreply.github.com>
2024-08-28 11:18:22 +0530

    Time: 62 ms (33.41%), Space: 36.5 MB (53.94%) - LeetHub

--- 2090-number-of-ways-to-arrive-at-destination/2090-number-of-ways-to-arrive-at-destination.cpp
+++ 2090-number-of-ways-to-arrive-at-destination/2090-number-of-ways-to-arrive-at-destination.cpp
@@ -1,38 +1,42 @@
+#define mod 1000000007
+
+
 class Solution {
 public:
-typedef long long int;
     int countPaths(int n, vector<vector<int>>& roads) {
-         int mod = (1e9+7);
-        vector<pair<int,int>> adj[n];
-        for (auto it : roads)
-        {
-            adj[it[0]].push_back({it[1], it[2]});   
-            adj[it[1]].push_back({it[0], it[2]});   // bcz of bidirectional edges
+        vector<pair<int, int>> adj[n];
+        for (auto i : roads) {
+            adj[i[0]].push_back({i[1], i[2]});
+            adj[i[1]].push_back({i[0], i[2]});
         }
-       // for(auto i:roads) adj[i[0]].push_back({i[1],i[2]});
-        vector<long long int> dist(n,1e18);
-        vector<long long int> ways(n,0);
-        priority_queue<pair<long long, long long>,vector<pair<long long,long long>>,greater<pair<long long ,long long>>> q; //{time,node}
-        q.push({0,0});
-        dist[0]=0;
-        ways[0]=1;
-        while(!q.empty()){
-            long long time=q.top().first;
-            int node=q.top().second;
+        
+        vector<long long> dist(n, 1e18);  // Initialize with a large value
+        vector<long long> ways(n, 0);     // Initialize ways to 0
+        priority_queue<pair<long, long>, vector<pair<long, long>>, greater<pair<long, long>>> q; // {time, node}
+        
+        dist[0] = 0;
+        ways[0] = 1;  // Start at node 0 with 1 way to get there
+        q.push({0, 0});
+        
+        while (!q.empty()) {
+            long time = q.top().first;
+            int node = q.top().second;
             q.pop();
-            for(auto it:adj[node]){
-                int adjnode=it.first;
-                long long edgW=it.second;
-                if(time+edgW<dist[adjnode]){
-                    dist[adjnode]=time+edgW;
-                    ways[adjnode]=ways[node];
-                    q.push({time+edgW,adjnode});
-                } else if(time+edgW==dist[adjnode]){
-                    ways[adjnode]=(ways[adjnode]+ways[node])%mod;
+            
+            for (auto i : adj[node]) {
+                int adjNode = i.first;
+                long long edgeW = i.second;
+                
+                if (time + edgeW < dist[adjNode]) {
+                    dist[adjNode] = time + edgeW;
+                    ways[adjNode] = ways[node];  // Update ways to reach adjNode
+                    q.push({dist[adjNode], adjNode});
+                } else if (time + edgeW == dist[adjNode]) {
+                    ways[adjNode] = (ways[adjNode] + ways[node]) % mod;  // Increment ways modulo mod
                 }
-               
             }
         }
-        return ways[n-1]%mod;
+        
+        return ways[n-1] % mod;  // Return the number of ways to reach the last node
     }
 };
