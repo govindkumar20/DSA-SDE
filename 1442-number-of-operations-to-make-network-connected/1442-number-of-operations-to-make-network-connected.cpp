84404ac7cf9f66b5fa78a655aa361cfd6298ba47
Govind Kumar <120386156+govindkumar20@users.noreply.github.com>
2024-08-29 14:54:01 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1442-number-of-operations-to-make-network-connected/1442-number-of-operations-to-make-network-connected.cpp
+++ 1442-number-of-operations-to-make-network-connected/1442-number-of-operations-to-make-network-connected.cpp
@@ -1,29 +1,27 @@
 class Solution {
 public:
-
-    void dfs(int i,vector<vector<int>>& adj, vector<int>& vis){
-        vis[i]=1;
-        for(auto it:adj[i]){
-            if(!vis[it]){
-                dfs(it,adj,vis);
-            }
+void dfs(int n, int node, vector<vector<int>>& adj,vector<vector<int>>& connections,vector<int>& vis ){
+    vis[node]=1;
+    for(auto i:adj[node]){
+        if(!vis[i]){
+            dfs(n,i,adj,connections,vis);
         }
     }
+}
     int makeConnected(int n, vector<vector<int>>& connections) {
-        if(connections.size()<n-1) return -1;
+        if(connections.size()!=n-1) return -1;
         vector<vector<int>> adj(n);
         for(auto i:connections){
-           adj[i[0]].push_back(i[1]);
-           adj[i[1]].push_back(i[0]);
-
+            adj[i[0]].push_back(i[1]);
+            adj[i[1]].push_back(i[0]);
         }
-        int count=0;
-        vector<int> vis(n,0);
+         vector<int> vis(n,0);
+         int count=0;
         for(int i=0;i<n;i++){
-            if(!vis[i]){
-                dfs(i,adj,vis);
-                count++;
-            }
+         if(!vis[i]){
+            dfs(n,i,adj,connections,vis);
+            count++;
+         }
         }
         return count-1;
     }
