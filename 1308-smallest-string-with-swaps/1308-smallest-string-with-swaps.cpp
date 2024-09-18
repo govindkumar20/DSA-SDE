57a395a45acbe8b8bc50063c5471eefdad67679a
Govind Kumar <120386156+govindkumar20@users.noreply.github.com>
2024-09-18 15:23:16 +0530

    Time: 97 ms (94.79%), Space: 52.5 MB (90.62%) - LeetHub

--- 1308-smallest-string-with-swaps/1308-smallest-string-with-swaps.cpp
+++ 1308-smallest-string-with-swaps/1308-smallest-string-with-swaps.cpp
@@ -1,59 +1,74 @@
-
 class disjoint{
-    public:
+public:
     vector<int> parent;
     vector<int> size;
+
     disjoint(int n){
         parent.resize(n+1);
         size.resize(n+1);
-        for(int i=0;i<n;i++){
-            parent[i]=i;
-            size[i]=i;
+        for(int i = 0; i < n; i++){
+            parent[i] = i;
+            size[i] = 1;  // Initialize the size of each set to 1
         }
     }
-        int findulp(int node){
-            if(parent[node]==node) return node;
-            return parent[node]=findulp(parent[node]);
+
+    int findulp(int node){
+        if(parent[node] == node) return node;
+        return parent[node] = findulp(parent[node]);  // Path compression
+    }
+
+    void unionbysize(int u, int v){
+        int ulp_u = findulp(u);
+        int ulp_v = findulp(v);
+        if(ulp_u == ulp_v) return;
+        
+        // Union by size to maintain efficiency
+        if(size[ulp_u] < size[ulp_v]){
+            parent[ulp_u] = ulp_v;
+            size[ulp_v] += size[ulp_u];
+        } else {
+            parent[ulp_v] = ulp_u;
+            size[ulp_u] += size[ulp_v];
         }
-        void unionbysize(int u,int v){
-            int ulp_u=findulp(u);
-            int ulp_v=findulp(v);
-            if(ulp_u==ulp_v) return;
-            else if(size[ulp_u]<size[ulp_v]){
-                parent[ulp_u]=ulp_v;
-                size[ulp_v]+=size[ulp_u];
-            } else{
-                 parent[ulp_v]=ulp_u;
-                size[ulp_u]+=size[ulp_v];
-            }
-     }
+    }
 };
 
 class Solution {
 public:
     string smallestStringWithSwaps(string s, vector<vector<int>>& pairs) {
-        int n=s.size();
+        int n = s.size();
         disjoint ds(n);
-        for(auto i:pairs){
-            ds.unionbysize(i[0],i[1]);
+
+        // Union the indices according to the pairs
+        for(auto& p : pairs){
+            ds.unionbysize(p[0], p[1]);
         }
-        map<int,vector<int>> m;
-        for(int i=0;i<n;i++){
+
+        // Group all indices that are in the same component
+        map<int, vector<int>> m;
+        for(int i = 0; i < n; i++){
             m[ds.findulp(i)].push_back(i);
         }
 
-        for(auto i:m){
-            vector<int> v=i.second;
-            string temp="";
-            for(auto j:i.second){
-                temp+=s[j];
-            }
-            sort(temp.begin(),temp.end());
-            for(int k=0;k<temp.size();k++){
-                s[v[k]]=temp[k];
+        // For each component, sort the characters and place them back in the string
+        for(auto& entry : m){
+            vector<int>& indices = entry.second;
+            string temp = "";
+
+            // Collect characters of the current component
+            for(auto& idx : indices){
+                temp += s[idx];
             }
 
+            // Sort the characters in the component
+            sort(temp.begin(), temp.end());
+
+            // Put sorted characters back in the original string
+            for(int k = 0; k < indices.size(); k++){
+                s[indices[k]] = temp[k];
+            }
         }
+
         return s;
     }
 };
